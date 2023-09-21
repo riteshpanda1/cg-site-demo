86454cd24df727b583f6a5c99f95f66eec890c22
Arja-Sankar <arja.rao@infosys.com>
2023-09-21 16:48:23 +0530

    disclosure.js added

---
+++ blocks/disclosure/disclosure.js
@@ -0,0 +1,38 @@
+//hiding doc content
+document.querySelector('.disclosure.block').style.display = 'none';
+//div creation starts
+var divName = document.querySelector('.disclosure-wrapper');
+divName.classList.add('responsivegrid', 'cg-max-width', 'aem-GridColumn', 'aem-GridColumn--default--12');
+var firstDiv = document.createElement('div');
+firstDiv.classList.add('aem-Grid', 'aem-Grid--12', 'aem-Grid--default--12');
+divName.appendChild(firstDiv);
+var secondDiv = document.createElement('div');
+secondDiv.classList.add('disclosure', 'parbase', 'aem-GridColumn', 'aem-GridColumn--default--12');
+firstDiv.appendChild(secondDiv);
+var thirdDiv = document.createElement('div');
+thirdDiv.setAttribute('class','noindex');
+secondDiv.appendChild(thirdDiv);
+var fourthDiv = document.createElement('p');
+fourthDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  fourthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[0]);
+  thirdDiv.appendChild(fourthDiv);
+ var fifthDiv = document.createElement('p');
+  fifthDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  fifthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[0]);
+    thirdDiv.appendChild(fifthDiv);
+  var sixthDiv = document.createElement('p');
+  sixthDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  sixthDiv.innerText = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[2].innerText;
+    thirdDiv.appendChild(sixthDiv);
+ var sevenDiv = document.createElement('p');
+  sevenDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  sevenDiv.innerText = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[3].innerText;
+    thirdDiv.appendChild(sevenDiv);
+  var eigthDiv = document.createElement('p');
+  eigthDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  eigthDiv.innerText = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[4].innerText;
+    thirdDiv.appendChild(eigthDiv);
+ var ninethDiv = document.createElement('p');
+  ninethDiv.classList.add('cg-global-disclosure-1', 'noindex');
+  ninethDiv.innerText = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[5].innerText;
+    thirdDiv.appendChild(ninethDiv);
