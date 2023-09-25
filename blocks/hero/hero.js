a522c6be47e96a55feac32a4efbff4dda64b0b68
riteshpanda1 <141994598+riteshpanda1@users.noreply.github.com>
2023-09-25 15:39:55 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,11 @@
 var firstDiv=document.querySelector(".hero-wrapper>.hero.block>div>div");
-var firstHeading=document.createElement("h1");
-firstHeading.setAttribute("id","textOne");
-firstHeading.innerText="Risks of bad breadth: Market concentration in 5 charts";
-firstDiv.appendChild(firstHeading);
+var firstElement=document.createElement("h1");
+firstElement.setAttribute("id","textOne");
+firstElement.innerText="Risks of bad breadth: Market concentration in 5 charts";
+firstDiv.appendChild(firstElement);
+
+secondElement=document.createElement("button");
+secondElement.setAttribute("class","hero-button");
+secondElement.innerText="Get Insights";
+firstDiv.appendChild(secondElement);
+
