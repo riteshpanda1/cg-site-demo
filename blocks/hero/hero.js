3900241bcf4c5150ece7b54bd5f8b9b0b285d105
riteshpanda1 <141994598+riteshpanda1@users.noreply.github.com>
2023-09-25 15:26:10 +0530

    Update hero.js

---
+++ blocks/hero/hero.js
@@ -0,0 +1,5 @@
+var firstDiv=document.querySelector(".hero-wrapper>.hero.block>div>div");
+var firstHeading=document.createElement("h1");
+firstHeading.setAttribute("id","textOne");
+firstHeading.innerText="Risks of bad breadth: Market concentration in 5 charts";
+fdiv.appendChild(firstHeading);
