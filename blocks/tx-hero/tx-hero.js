4ee88c4be0c5c9e892bb6e2580862a1d05566853
riteshpanda1 <141994598+riteshpanda1@users.noreply.github.com>
2023-09-25 16:40:15 +0530

    Update tx-hero.js

--- blocks/tx-hero/tx-hero.js
+++ blocks/tx-hero/tx-hero.js
@@ -1,1 +1,10 @@
+var firstDiv=document.querySelector(".tx-hero-wrapper>.tx-hero.block>div>div");
+var firstElement=document.createElement("h1");
+firstElement.setAttribute("id","textOne");
+firstElement.innerText="Risks of bad breadth: Market concentration in 5 charts";
+firstDiv.appendChild(firstElement);
 
+var secondElement=document.createElement("button");
+secondElement.setAttribute("class","hero-button");
+secondElement.innerText="Get Insights";
+firstDiv.appendChild(secondElement);
